592e896f54d608d3989ceaa804fd3bd0c5530b32
chmi <294225707@qq.com>
2020-01-10 10:41:10 +0800

    添加接口文档

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "接口文档",
+  "version": "1.0.0",
+  "description": "接口文档",
+  "title": "接口文档",
+  "url": "http://192.168.50.179:280/api",
+  "sampleUrl": "http://192.168.50.179:280/api",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-11-22T07:21:21.316Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
